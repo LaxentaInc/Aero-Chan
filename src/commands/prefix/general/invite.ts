2c38a58db000b00401e7b52e74b1a349a8117910
LaxentaInc <gk559850@gmail.com>
2026-04-29 10:33:40 +0530

    ts migra: invite.ts

---
+++ src/commands/prefix/general/invite.ts
@@ -0,0 +1,20 @@
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle } from "discord.js";
+export default {
+  name: 'invite',
+  description: 'Invite the bot or join the server using buttons.',
+  async execute(message: any) {
+    try {
+      // Define buttons for inviting the bot and joining the server
+      const row = new ActionRowBuilder().addComponents(new ButtonBuilder().setLabel('Invite the Bot').setStyle(ButtonStyle.Link).setURL('https://discord.com/oauth2/authorize?client_id=1107155830274523136&permissions=1118435113046&scope=bot%20applications.commands'), new ButtonBuilder().setLabel('Join our devs').setStyle(ButtonStyle.Link).setURL('https://discord.gg/9emnU25HaY'));
+
+      // Send the message with buttons
+      await message.channel.send({
+        content: 'Click the buttons below to invite the bot or join the server!',
+        components: [row]
+      });
+    } catch (error: any) {
+      console.error('Error sending invite buttons:', error);
+      await message.channel.send('An error occurred while generating the invite buttons. Please try again later.');
+    }
+  }
+};
