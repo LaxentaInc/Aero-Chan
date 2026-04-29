ddf46e46a8a8f43d9abe802c5ebc5db8d019c3d7
LaxentaInc <gk559850@gmail.com>
2026-04-29 10:34:29 +0530

    ts migra: AMA.ts

---
+++ src/modules/AMA.ts
@@ -0,0 +1,8 @@
+/**
+ * Anti Mass Action (AMA) - Legacy Wrapper
+ * 
+ * This file serves as a wrapper to load the modular AMA system.
+ * Logic has been moved to src/modules/helpers/ama/
+ */
+import _reexport from "./helpers/ama";
+export default _reexport;
