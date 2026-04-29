6d9275a8cc89da0c8608e9bdd514e7efd29ada6e
LaxentaInc <gk559850@gmail.com>
2026-04-29 10:34:09 +0530

    ts migra: smug.ts

---
+++ src/commands/slash/Others/smug.ts
@@ -0,0 +1,63 @@
+import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
+import axios from "axios";
+import fs from "fs";
+import path from "path";
+// ========== counter ==========
+const STATS_PATH = path.join(__dirname, '../../../data/smug_stats.json');
+const loadStats = () => {
+  try {
+    if (!fs.existsSync(STATS_PATH)) {
+      fs.writeFileSync(STATS_PATH, '{}', 'utf8');
+      return {};
+    }
+    return JSON.parse(fs.readFileSync(STATS_PATH, 'utf8'));
+  } catch {
+    return {};
+  }
+};
+const saveStats = (s: any) => {
+  try {
+    fs.writeFileSync(STATS_PATH, JSON.stringify(s, null, 2), 'utf8');
+  } catch (e: any) {
+    console.error('failed to save smug stats:', e);
+  }
+};
+const incrementCount = (id1: any, id2: any) => {
+  const s = loadStats();
+  const k = [id1, id2].sort().join(':');
+  s[k] = (s[k] || 0) + 1;
+  saveStats(s);
+  return s[k];
+};
+export default {
+  data: new SlashCommandBuilder().setName('smug').setDescription('Smug face~').setIntegrationTypes(0, 1).setContexts(0, 1, 2).addUserOption((option: any) => option.setName('target').setDescription('The user to be smug at').setRequired(false)),
+  async execute(interaction: any) {
+    const target = interaction.options.getUser('target');
+    await interaction.deferReply();
+    try {
+      const response = await (axios.get('https://nekos.best/api/v2/smug', {
+        timeout: 5000
+      }) as any);
+      const gif = response.data.results[0].url;
+      if (!gif) return interaction.editReply({
+        content: 'couldn\'t be smug. try again!'
+      });
+      let desc;
+      if (target && target.id !== interaction.user.id) {
+        const count = incrementCount(interaction.user.id, target.id);
+        desc = `-# **${interaction.user.username}** is being smug at **${target.username}~**\n-# ***${interaction.user.username}** has smugged at **${target.username}** ${count} ${count === 1 ? 'time' : 'times'}.*`;
+      } else {
+        desc = `-# **${interaction.user.username}** is being smug~`;
+      }
+      const embed = new EmbedBuilder().setDescription(desc).setImage(gif);
+      await interaction.editReply({
+        embeds: [embed]
+      });
+    } catch (e: any) {
+      console.error(`error: ${e.message}`);
+      await interaction.editReply({
+        content: 'failed to be smug!'
+      });
+    }
+  }
+};
