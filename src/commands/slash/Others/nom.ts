2e846becce4ab93a48074592a0223e1ec45dcc65
LaxentaInc <gk559850@gmail.com>
2026-04-29 10:34:05 +0530

    ts migra: nom.ts

---
+++ src/commands/slash/Others/nom.ts
@@ -0,0 +1,26 @@
+import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
+import axios from "axios";
+export default {
+  data: new SlashCommandBuilder().setName('nom').setDescription('Nom nom nom~').setIntegrationTypes(0, 1).setContexts(0, 1, 2),
+  async execute(interaction: any) {
+    await interaction.deferReply();
+    try {
+      const response = await (axios.get('https://nekos.best/api/v2/nom', {
+        timeout: 5000
+      }) as any);
+      const gif = response.data.results[0].url;
+      if (!gif) return interaction.editReply({
+        content: 'nothing to nom!'
+      });
+      const embed = new EmbedBuilder().setDescription(`-# **${interaction.user.username}** is nomming~ nom nom nom!`).setImage(gif);
+      await interaction.editReply({
+        embeds: [embed]
+      });
+    } catch (e: any) {
+      console.error(`error: ${e.message}`);
+      await interaction.editReply({
+        content: 'failed to nom!'
+      });
+    }
+  }
+};
