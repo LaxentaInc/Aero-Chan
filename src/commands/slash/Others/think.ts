ddd7c6662af1854b2ac98ebaf54f8fc221c90129
LaxentaInc <gk559850@gmail.com>
2026-04-29 10:34:10 +0530

    ts migra: think.ts

---
+++ src/commands/slash/Others/think.ts
@@ -0,0 +1,26 @@
+import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
+import axios from "axios";
+export default {
+  data: new SlashCommandBuilder().setName('think').setDescription('Hmm... let me think about that...').setIntegrationTypes(0, 1).setContexts(0, 1, 2),
+  async execute(interaction: any) {
+    await interaction.deferReply();
+    try {
+      const response = await (axios.get('https://nekos.best/api/v2/think', {
+        timeout: 5000
+      }) as any);
+      const gif = response.data.results[0].url;
+      if (!gif) return interaction.editReply({
+        content: 'couldn\'t think. try again!'
+      });
+      const embed = new EmbedBuilder().setDescription(`-# **${interaction.user.username}** is thinking...`).setImage(gif);
+      await interaction.editReply({
+        embeds: [embed]
+      });
+    } catch (e: any) {
+      console.error(`error: ${e.message}`);
+      await interaction.editReply({
+        content: 'failed to think!'
+      });
+    }
+  }
+};
