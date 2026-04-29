252baf97f0930154dfeb24b33446d1374efe2203
LaxentaInc <gk559850@gmail.com>
2026-04-29 10:33:46 +0530

    ts migra: unlock.ts

---
+++ src/commands/prefix/moderations/unlock.ts
@@ -0,0 +1,21 @@
+import { PermissionsBitField } from "discord.js";
+export default {
+  name: "unlock",
+  description: "Unlocks the specified channel or the current channel if no argument is provided.",
+  usage: "!unlock [channel]",
+  async execute(message: any, args: any) {
+    if (!message.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+      return message.reply("You do not have permission to use this command.");
+    }
+    const channel = message.mentions.channels.first() || message.channel;
+    await unlockChannel(channel);
+    await message.reply(`Channel ${channel.name} has been unlocked.`);
+  }
+};
+async function unlockChannel(channel: any) {
+  await channel.permissionOverwrites.edit(channel.guild.roles.everyone, {
+    ViewChannel: true,
+    SendMessages: true
+  });
+  console.log(`Unlocked channel ${channel.name} in guild ${channel.guild.id}`);
+}
