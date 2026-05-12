bf63ca3d6e350e0cb54f1683439ad96420d58bbe
LaxentaInc <gk559850@gmail.com>
2026-05-12 21:10:33 +0530

    Update src/modules/helpers/apa/punishment.ts

--- src/modules/helpers/apa/punishment.ts
+++ src/modules/helpers/apa/punishment.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { PermissionFlagsBits } from "discord.js";
 import { DANGEROUS_PERMISSIONS } from "./config";
 import { saveUserStrip, loadUserStrip, clearUserStrip } from "./storage";
