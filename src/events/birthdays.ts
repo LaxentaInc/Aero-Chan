833d5a67ba1797a8d9a3e800f429787051e6cc2a
LaxentaInc <gk559850@gmail.com>
2026-05-12 21:10:22 +0530

    Update src/events/birthdays.ts

--- src/events/birthdays.ts
+++ src/events/birthdays.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import cron from "node-cron";
 import moment from "moment-timezone";
 import { EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle } from "discord.js";
